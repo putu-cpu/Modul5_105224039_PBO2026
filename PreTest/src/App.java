f713ecefe38600b771cbeca69e1d5d7019479f23
Haruka <putuharu926@gmail.com>
2026-04-02 15:20:03 +0700

    first commit

---
+++ PreTest/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        rekeningBank rekening = new rekeningBank()
+        rekening.setor();
+    }
+}
+
---
+++ PreTest/src/rekeningBank.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class rekeningBank {
+    private int saldo;
+    private Scanner input;
+    
+    public void setor () {
+        
+        input = new Scanner(System.in);
+        System.out.println("Masukkan saldo anda :");
+        int jumlah = this.scanner.nextint(); 
+        saldo + jumlah ; 
+
+        if (jumlah < 0 ) {
+            System.out.println("tidak boleh min ");
+        } else {
+            System.out.println("Selamat anda berhasil ditambahkan");
+        }
+
+
+    }
+}
